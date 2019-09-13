76d2c1c4b60297df969e35da5a476d55c30fe25d
Denis Kotlica <neosiae@protonmail.com>
2019-09-13 14:55:39 +0200

    Add: build

---
+++ build.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+const projects = require('./content/projects.json')
+
+// Homepage
+const indexTemplate = fs.readFileSync(path.join(__dirname, 'templates/index.html'), { encoding: 'utf8' })
+
+const projectsMarkup = projects
+  .map(project => `<li><a href="${project.url}">${project.name}</a></li>`)
+  .join('\n')
+
+const index = indexTemplate.replace(/{projects}/, projectsMarkup)
+
+fs.writeFileSync(path.join(__dirname, 'index.html'), index)
